cc0c2a5a2acbc9044c2d01ecc4e26bc0353282ee
Muhmmd-Usama <mummdusama@gmail.com>
2022-08-21 11:24:29 +0200

    Slideshow finsh

--- Slideshow/index.js
+++ Slideshow/index.js
@@ -1,1 +1,20 @@
+let img = document.getElementById('slideshow'),
+  images = ['./image/1.jpg', './image/2.png', './image/3.jpg', './image/4.jpg'];
+let i = 0;
+//src="./image/1.jpg"
 
+function slide() {
+    console.log('log')
+    img.setAttribute('src', images[i]);
+    i++
+    if(i== images.length){
+        i=0
+    }
+  
+
+  setTimeout(function () {
+    slide();
+  }, 1000);
+}
+
+slide();

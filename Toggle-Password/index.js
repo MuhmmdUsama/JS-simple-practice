f207d45a575f3b5ce100aa25eb9d6b8203cb5d64
Muhmmd-Usama <mummdusama@gmail.com>
2022-08-21 08:57:12 +0200

    Toggle Password finsh

--- Toggle-Password/index.js
+++ Toggle-Password/index.js
@@ -1,1 +1,15 @@
+let myInput = document.getElementById('myInput'),
+  btn = document.getElementById('btn');
+  btn.setAttribute('data-text','show')
 
+  btn.onclick = function(){
+    if(btn.getAttribute('data-text') == 'show'){
+        this.innerHTML = 'Hide'
+        this.setAttribute('data-text','hide')
+        myInput.setAttribute('type','text')
+    }else{
+        this.innerHTML = 'Show'
+        this.setAttribute('data-text','show')
+        myInput.setAttribute('type','password')
+    }
+  }
